8bbf90a7b2c458672bdfa7900042f39285cf5076
zhayden14 <44064251+zhayden14@users.noreply.github.com>
2021-06-12 14:56:10 -0600

    start setup for venv
    this can be used to create a virtual environment. but it really needs to be cleaned up at some point

---
+++ Python/Tools/qwik_setup.bat
@@ -0,0 +1,9 @@
+
+
+:: will eventually be replaced by a setuptools-based system of creating a virtual environment
+
+python -m venv .\.venv
+
+.\.venv\Scripts\activate.bat
+
+python -m pip install -r requirements.txt
